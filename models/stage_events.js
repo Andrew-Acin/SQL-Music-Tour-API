4459cc78687a33a24ef3b77853267f7877baa057
Andrew Acin <andrew_acin@hotmail.com>
2024-07-07 18:26:46 -0700

    finished adding tables

---
+++ models/stage_events.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class stage_events extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  stage_events.init({
+    stage_events_id: {
+      allowNull: false,
+      type:DataTypes.INTEGER
+
+    },
+    stage_id: {
+      allowNull: false,
+      type:DataTypes.INTEGER
+    },
+    event_id: {
+      allowNull: false,
+      type:DataTypes.INTEGER
+    }
+  }, {
+    sequelize,
+    modelName: 'stage_events',
+    tableName: 'stage_events',
+    timestamps: false
+  });
+  return stage_events;
+};
