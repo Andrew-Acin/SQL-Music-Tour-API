c6d990e868bb27c6fd92292040258d17b31d40ae
Andrew Acin <andrew_acin@hotmail.com>
2024-07-10 20:14:57 -0700

    commit class 6

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db 
+const { Band, MeetGreet, Event, SetTime } = db 
 const { Op } = require('sequelize')
 
 // FIND ALL BANDS
@@ -19,11 +19,22 @@ bands.get('/', async (req, res) => {
     }
 })
 
-// FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
+// FIND A SPECIFIC BAND -- SHOW ROUTE
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
+            where: { name: req.params.name },
+                include: [{ 
+                    model: MeetGreet, 
+                    as: "meet_greets", 
+                    include: {model: Event, as: "event"}
+                },
+                {
+                    model: SetTime,
+                    as: "set_times",
+                    include: {model: Event, as: "event"}
+                }
+            ]
         })
         res.status(200).json(foundBand)
     } catch (error) {
--- models/event.js
+++ models/event.js
@@ -4,13 +4,25 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class event extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({Stage, StageEvent, SetTimes, MeetGreet}) {
+      // Stages
+      event.belongsToMany(Stage, {
+        foreignKey: "event_id",
+        as: "stages",
+        through: StageEvent
+      })
+
+      // meetGreets
+    event.hasMany(MeetGreet, {
+        foreignKey: "event_id",
+        as: "meet_greets"
+      })
+
+      // set times
+      event.hasMany(SetTimes, {
+        foreignKey: "event_id",
+        as: "set_times"
+      })
     }
   }
   event.init({
